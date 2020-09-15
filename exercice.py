1336802bf43e61655b5056a1cc8830c17d13c926
Rakotomalala01 <johary.rakotomalala@polymtl.ca>
2020-09-15 23:26:04 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+https://teams.microsoft.com/l/channel/19%3ac99a11edf1344df3be675b2052998498%40thread.tacv2/Cours%2520C04?groupId=0f3c8839-4918-45bf-b83d-a9b2128eb0c7&tenantId=d2ee49eb-e94b-49d7-a34a-2fb97a949bfc
 
 
 
@@ -14,8 +15,7 @@ def remove_third_char(string: str) -> str:
     return string[0:2]+string[3:]
 
 
-def replace_char(string: str, old_char: str, new_char: str) -> str:
-    old char=https://teams.microsoft.com/l/channel/19%3ac99a11edf1344df3be675b2052998498%40thread.tacv2/Cours%2520C04?groupId=0f3c8839-4918-45bf-b83d-a9b2128eb0c7&tenantId=d2ee49eb-e94b-49d7-a34a-2fb97a949bfc
+def replace_char(string: str, old_char: str, new_char: str) -> str:s%2520C04?groupId=0f3c8839-4918-45bf-b83d-a9b2128eb0c7&tenantId=d2ee49eb-e94b-49d7-a34a-2fb97a949bfc
 
 
 def get_number_of_char(string: str, char: str) -> int:

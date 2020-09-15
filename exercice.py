c19cbb32a5414a9d66b14cb0364b42784a55c10c
Rakotomalala01 <johary.rakotomalala@polymtl.ca>
2020-09-15 22:57:09 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,7 @@ def is_even_len(string: str) -> bool:
 
 def remove_third_char(string: str) -> str:
     pass
-    
+    strin
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:

7fd3a165414f77ad1b47c46cf7ed8b052669075a
Rakotomalala01 <johary.rakotomalala@polymtl.ca>
2020-09-15 23:41:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str):
     for i in range(len(string)):
-        if string
+        if string[i]
 
 def get_number_of_char(string: str, char: str) -> int:
     pass

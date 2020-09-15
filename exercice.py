0921ee4adb0053b7b410a895499c27986b105a2f
Rakotomalala01 <johary.rakotomalala@polymtl.ca>
2020-09-15 23:47:22 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,7 +23,7 @@ def replace_char(string: str, old_char: str, new_char: str):
 def get_number_of_char(string: str, char: str) -> int:
     pass
 for i in range (len string):
-    if 
+    
 
 
 def get_number_of_words(sentence: str, word: str) -> int:

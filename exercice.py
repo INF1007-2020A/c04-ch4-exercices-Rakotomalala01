4d7d63564ead52751d18c7b1cd61f34be5aae267
Rakotomalala01 <johary.rakotomalala@polymtl.ca>
2020-09-21 22:41:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -32,8 +32,7 @@ for i in range (len string):
 def get_number_of_words(sentence: str, word: str) -> int:
     pass
     nbr word =0
-for i in range (len sentence):
-    
+
 def main()-> None:
     chaine = "Bonjour!"
     if is_even_len(chaine):
